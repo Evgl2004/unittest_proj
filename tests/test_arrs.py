715558dcbf8783144e7432da664de5279f93e891
Andrey Evgl <evgl2004@yandex.ru>
2023-07-25 09:32:02 +0500

    Внесены изменения в тесты "test_slice"

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,3 +11,6 @@ class TestArrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([99, 1, 100], -55), [99, 1, 100])
+        self.assertEqual(arrs.my_slice(['f', ',', 'd', 'u'], -2), ['d', 'u'])
+        self.assertEqual(arrs.my_slice([], 1), [])
